2fc6cdd8f373c982a9dd238dfdb48b03c6b7cd19
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-22 23:55:06 +0700

    Implement db with quizIndex and wordText

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,14 +4,51 @@
     class="h-screen overflow-hidden">
     <div class="h-full flex items-center justify-center bg-primary text-white _fs-25vh">
       <div class="w-10/12 break-all text-center">
-        Titanic
+        {{ wordText }}
       </div>
     </div>
+    <div
+      class="fixed w-6/12 h-full top-0"
+      @click="onCorrect" />
+    <div
+      class="fixed w-6/12 h-full top-0 left-1/2"
+      @click="onNextWord" />
   </div>
 </template>
 
 <script>
+import db from '../../public/db.json'
+
 export default {
-  name: 'GamePage'
+  name: 'GamePage',
+  props: {
+    quizIndex: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+  data () {
+    return {
+      wordIndex: 0,
+      score: 0
+    }
+  },
+  computed: {
+    words () {
+      return db[this.quizIndex].words
+    },
+    wordText () {
+      return this.words[this.wordIndex].text
+    }
+  },
+  methods: {
+    onNextWord () {
+      this.wordIndex += 1
+    },
+    onCorrect () {
+      this.score += 1
+      this.onNextWord()
+    }
+  }
 }
 </script>
