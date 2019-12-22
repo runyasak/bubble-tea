2ef53c29856daade87ef5132acf2086e36335467
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 05:27:54 +0700

    Created Title screen component.

---
+++ src/components/GameScreen/TitleScreen/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>Bubble Tea</span>
+    <button>Start Game</button>
+    <button>Edit Word</button>
+    <button>View Scores</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
