b3b274ad1c27a206416e4db8b47ea29633f944fe
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 06:20:06 +0700

    Updated Word Selection screen to show word categories for selection.

--- src/components/GameScreen/WordSelectionScreen/index.vue
+++ src/components/GameScreen/WordSelectionScreen/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-      Word Selection
+    Word Selection
+    {{ wordCategories }}
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    gameData: {
+      type: Function,
+      default() {
+        return {
+          content: {}
+        }
+      }
+    }
+  },
+  computed: {
+    wordCategories() {
+      let wordCategories
+      const gameData = this.gameData()
+      if ('content' in gameData) {
+        delete gameData.content.mode
+        const categories = Object.keys(this.gameData().content)
+        wordCategories = categories.map((category) => gameData.content[category].title)
+      }
+      return wordCategories
+    }
+  }
 }
 </script>
 
