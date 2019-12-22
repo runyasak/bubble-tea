9128150f878744ca4b75eb52bd90924764ae68da
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-22 19:48:21 +0700

    Setup colors in tailwind.js

--- tailwind.js
+++ tailwind.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: '#3498DB',
+      secondary: '#F1C40F',
+      neutral: '#34495E',
+      success: '#2ECC71',
+      danger: '#E74C3C'
+    }
   },
   variants: {},
   plugins: []
