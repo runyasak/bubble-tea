840dea64885acaf6e1bc8c84b2927e2fceadf277
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 02:55:56 +0700

    Updated game content retrieving logic.

--- src/logics/index.js
+++ src/logics/index.js
@@ -1,23 +1,35 @@
 import axios from 'axios'
-
 /* eslint-disable no-use-before-define */
 
 export default {
-  fetchGameContent
+  getGameContent
+}
+
+async function* getGameContent() {
+  const localContent = JSON.parse(localStorage.getItem('content')) || null
+  if (localContent) {
+    yield { ...localContent, mode: 'local' }
+  }
+  const updatedContent = await updateGameContentDownstream(localContent)
+  yield { ...updatedContent, mode: 'updated' }
+}
+
+async function updateGameContentDownstream(localContent) {
+  const newGameContent = await fetchGameContent()
+  const gameContent = { ...localContent, ...newGameContent }
+  localStorage.setItem('content', JSON.stringify(gameContent))
+  return gameContent
 }
 
 async function fetchGameContent() {
-  let content = JSON.parse(localStorage.getItem('content'))
-  if (!content) {
-    try {
-      const result = await axios.get('/db.json')
-      if (result.status === 200) {
-        content = result.data
-        localStorage.setItem('content', JSON.stringify(content))
-      }
-    } catch (e) {
-      console.log(e)
+  let content = {}
+  try {
+    const result = await axios.get('/db.json')
+    if (result.status === 200) {
+      content = result.data
     }
+  } catch (e) {
+    console.log(e)
   }
-  return content || {}
+  return content
 }
