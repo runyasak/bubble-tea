1b33ca225b2b72213fc75f7ad89f3a67b7237bb8
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-22 22:21:58 +0700

    Add Game page

---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    id="game-page"
+    class="h-screen overflow-hidden">
+    <div class="h-full flex items-center justify-center bg-primary text-white _fs-25vh">
+      <div class="w-10/12 break-all">
+        Titanic
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GamePage'
+}
+</script>
