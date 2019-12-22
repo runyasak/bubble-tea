02bccf81cbbc2e379ae5a76e40177c7289829a2e
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-22 19:02:56 +0700

    Implement yarn and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'airbnb-base'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'import/no-unresolved': 'off',
+    'space-before-function-paren': 'off',
+    'object-curly-newline': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'state',
+        'el'
+      ]
+    }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
