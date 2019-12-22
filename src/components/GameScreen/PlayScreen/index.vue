2c2242f282b3a62fd758e8fe8538bbd0a0d91d19
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 05:38:55 +0700

    Created Play screen component.

---
+++ src/components/GameScreen/PlayScreen/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="h-full flex items-center justify-center bg-primary text-white _fs-25vh">
+      <div class="w-10/12 break-all text-center">
+        WORD
+      </div>
+    </div>
+    <div
+      class="fixed w-6/12 h-full top-0"
+    >
+      Correct
+    </div>
+    <div
+      class="fixed w-6/12 h-full top-0 left-1/2"
+    >
+      Wrong
+    </div>
+  </div>
+</template>
