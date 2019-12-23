7d0545ef72d273b089b163e9c97de554e6b6d48e
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 07:22:47 +0700

    Updated summary screen to show score.

--- src/components/GameScreen/SummaryScreen/index.vue
+++ src/components/GameScreen/SummaryScreen/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    Total Score
+    Total Score {{ score }}
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    gameData: {
+      type: Function,
+      default() {
+        return {
+          score: 0
+        }
+      }
+    }
+  },
+  computed: {
+    score() {
+      const gameData = this.gameData()
+      return gameData.score
+    }
+  }
 }
 </script>
 
