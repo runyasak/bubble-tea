823c422e170f2f79d821b09d652956714e5fe4e0
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 08:25:33 +0700

    Created a new logic to update game content locally.

--- src/logics/index.js
+++ src/logics/index.js
@@ -3,7 +3,24 @@ import axios from 'axios'
 /* eslint-disable no-use-before-define */
 
 export default {
-  fetchGameContent
+  getGameContent,
+  updateGameContentLocally
+}
+
+async function* getGameContent() {
+  const localContent = JSON.parse(localStorage.getItem('content')) || null
+  if (localContent) {
+    yield { ...localContent, mode: 'local' }
+  }
+  const updatedContent = await updateGameContentDownstream(localContent)
+  yield { ...updatedContent, mode: 'updated' }
+}
+
+async function updateGameContentDownstream(localContent) {
+  const newGameContent = await fetchGameContent()
+  const gameContent = { ...localContent, ...newGameContent }
+  localStorage.setItem('content', JSON.stringify(gameContent))
+  return gameContent
 }
 
 async function fetchGameContent() {
@@ -20,4 +37,10 @@ async function fetchGameContent() {
     }
   }
   return content || {}
+}
+
+function updateGameContentLocally(key, content) {
+  const currentGameContent = JSON.parse(localStorage.getItem('content'))
+  currentGameContent[key] = content
+  localStorage.setItem('content', JSON.stringify(currentGameContent))
 }
