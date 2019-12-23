ad10537b421560e16fb20d7e55c854f998d143f2
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-24 02:38:49 +0700

    Add sticky nav in App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <nav
+      v-if="$route.name !== 'game'"
+      class="sticky top-0 flex items-center justify-between flex-wrap bg-primary p-2">
+      <div class="container mx-auto text-center text-4xl">
+        <span class="text-white font-bold">Bubble</span>
+        <span class="text-secondary font-bold"> Tea</span>
+        <span class="text-white font-bold"> .</span>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
