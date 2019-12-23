eaaee904c3f933df63c06642c80a8d1a9ccba6e6
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-12-23 07:28:09 +0700

    Created Create New Quiz screen component.

---
+++ src/components/GameScreen/CreateNewQuizScreen/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Create New Quiz
+    <div>Quiz Name</div>
+    <input v-model="quizName">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quizName: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
