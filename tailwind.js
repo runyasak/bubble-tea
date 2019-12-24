e627c8e91011f4e77b89f5942aaf832ad9758b63
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-25 00:49:44 +0700

    Add input component in plugins

--- tailwind.js
+++ tailwind.js
@@ -20,5 +20,17 @@ module.exports = {
     }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    ({ addComponents }) => {
+      const input = {
+        input: {
+          '&:focus': {
+            'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06) !important'
+          }
+        }
+      }
+
+      addComponents(input)
+    }
+  ]
 }
