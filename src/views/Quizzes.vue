7793f8c3083049b5972266badb6f295671e88f14
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-24 23:16:44 +0700

    Update to create card

--- src/views/Quizzes.vue
+++ src/views/Quizzes.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="quizzes-page">
     <div class="container mx-auto p-4">
-      <div class="flex justify-end pb-4">
-        <router-link
-          :to="{ name: 'create' }"
-          class="min-w-25 ml-auto bg-primary hover:bg-white
-          border border-primary
-          hover:text-primary text-white font-bold py-2 px-4 rounded-full">
-          + สร้างคำถาม
-        </router-link>
-      </div>
       <div class="flex flex-wrap -m-2">
+        <div class="md:flex w-full md:w-1/3 p-2">
+          <router-link
+            :to="{ name: 'create' }"
+            class="rounded block shadow-lg
+              py-12 px-6 w-full text-primary text-center text-2xl
+              border border-primary">
+            + สร้างคำถาม
+          </router-link>
+        </div>
         <div
           v-for="(quiz, index) in quizzes"
           :key="index"
